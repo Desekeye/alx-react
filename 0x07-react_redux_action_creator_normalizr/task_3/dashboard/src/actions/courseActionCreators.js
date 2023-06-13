ea1f2fa14af6bc2c6f78701ac21924fc109d8377
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-06-14 01:24:00 +0200

    Update courseActionCreators.js

--- 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.js
@@ -1,1 +1,15 @@
-ii
+import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+
+export function selectCourse(index) {
+  return {
+    type: SELECT_COURSE,
+    index: index,
+  };
+}
+
+export function unSelectCourse(index) {
+  return {
+    type: UNSELECT_COURSE,
+    index: index,
+  };
+}
